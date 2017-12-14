ae3e37951caeca70240fbdc9e231a3d9b882bdad
Dan Schneider <Dschnd@gmail.com>
2017-12-14 10:29:51 -0600

    Uploaded PHP Final Project

---
+++ php_Final_Project/phpFinal/index.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Ankeny Summerfest</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+
+<body>
+
+
+
+            <div class="home-header-bg">
+        <nav>
+                <div class="navMenu">
+                    <ul class="navList">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="#">Theme</a></li>
+                        <li><a href="#">Sponsors</a></li>
+                        <li><a href="get-involved.php">Get Involved</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                        <li><a href="admin/admin.php">Admin</a></li>
+                    </ul>
+                </div>
+        </nav>
+                <div class="home-header-content">
+                    <h1 class="header-title"><span class="ankeny">Ankeny</span> <br> <span class="summerfest">Summerfest</span></h1>
+                    <p class="lead">July 12-15, 2018</p>
+
+                </div>
+            </div>
+
+    <main id="main" >
+        <div class="container">
+            <div class="surveySection">
+                <div>
+                <h2 class="section-title">2017 in Review!</h2>
+                </div>
+                <div>
+                    <button>Take The Survey</button>
+                </div>
+            </div>
+            <div class="getInvolvedSection">
+                <h2 class="section-title">Get Involved!</h2>
+                <p>You are summerfest. Thank you for all you do!</p>
+                <button>Learn More.</button>
+            </div>
+            <div class="aboutSection">
+                <h2 class="section-title">About Ankeny SummerFest</h2>
+                <p class="lead">A tradition of summer fun...</p>
+                <p>Ankeny's annual SummerFest is a four-day celebration with carnival rides, parades, bands, family fun and more! Mark your calendars for SummerFest 2018 - Thursday through Sunday, July 12-15, 2018.</p>
+            </div>
+            <div class="meetTeamSection">
+                <h2 class="section-title">Meet The Team</h2>
+                <p class="lead">Just a few of the SummerFest faces</p>
+                <div class="portraitContainer">
+                    <div class="portraitName">
+                        <h3>Jessica Erickson</h3>
+                    </div>
+                    <div class="portraitImage">
+                        <img src="">
+                    </div>
+                    <div class="portraitDescription">
+                        <p> Events Director, Ankeny Area Chamber of Commerce </p>
+                    </div>
+                </div>
+                <!-- portraitContainer -->
+                <div class="portraitContainer">
+                    <div class="portraitName">
+                        <h3>Jeff Baxter</h3>
+                    </div>
+                    <div class="portraitImage">
+                        <img src="">
+                    </div>
+                    <div class="portraitDescription">
+                        <p>  SummerFest Chair, Dickinson, Mackaman, Tyler &amp; Hagen, P.C.  </p>
+                    </div>
+                </div>
+                <!-- portraitContainer -->
+                <div class="portraitContainer">
+                    <div class="portraitName">
+                        <h3>Larry Anderson</h3>
+                    </div>
+                    <div class="portraitImage">
+                        <img src="">
+                    </div>
+                    <div class="portraitDescription">
+                        <p>SummerFest Co-Chair, Central Bank</p>
+                    </div>
+                </div>
+                <!-- portraitContainer -->
+                <div class="portraitContainer">
+                    <div class="portraitName">
+                        <h3> Julie C. Todtz </h3>
+                    </div>
+                    <div class="portraitImage">
+                        <img src="">
+                    </div>
+                    <div class="portraitDescription">
+                        <p>  President/CEO, Ankeny Area Chamber of Commerce  </p>
+                    </div>
+                </div>
+                <!-- portraitContainer -->
+            </div>
+
+        </div>
+        <!-- container -->
+
+    </main>
+    <footer id="footer" >
+
+    </footer>
+</body>
+
+</html>
